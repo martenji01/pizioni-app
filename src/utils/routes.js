052c3d63aba229033f4d6c538e5b9fce25bca506
martenji01 <martacrd01@gmail.com>
2023-01-19 11:16:46 +0100

    routes

---
+++ src/utils/routes.js
@@ -0,0 +1,10 @@
+export const routes = {
+    HOME: "home",
+    RULES: "rules",
+    LOGIN: "login",
+    REGISTER: "register",
+    LEADERBOARDS: "leaderboards",
+    PLAY: "play",
+    GAME: "game"
+
+}
